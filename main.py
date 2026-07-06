14509a05de4806255b2dae44a7fa1dc59e7415b6
Roshni <roshniangral08@gmail.com>
2026-07-06 21:54:37 +0530

    Assign-3 Commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+books = [
+    {
+        "id": 1,
+        "title": "The Guide",
+        "author": "R K Narayan",
+        "genre": "Fiction",
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "Wings of Fire",
+        "author": "A P J Abdul Kalam",
+        "genre": "Biography",
+        "language": "English"
+    }
+]
+
+
+# Pydantic Model
+class BookCreate(BaseModel):
+    title: str
+    author: str
+    genre: str
+    language: str
+
+
+# Home API
+@app.get("/")
+def home():
+    return {"message": "Library API is running"}
+
+
+# Get All Books
+@app.get("/books")
+def get_books():
+    return books
+
+
+# Add New Book
+@app.post("/books", status_code=201)
+def create_book(book: BookCreate):
+
+    new_id = max([book["id"] for book in books], default=0) + 1
+
+    new_book = {
+        "id": new_id,
+        "title": book.title,
+        "author": book.author,
+        "genre": book.genre,
+        "language": book.language
+    }
+
+    books.append(new_book)
+
+    return {
+        "message": "Book added successfully",
+        "book": new_book
+    }
